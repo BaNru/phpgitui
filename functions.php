83a9a62d46a1a531df66daa35343390e144c00d0
BaNru <admin@g63.ru>
2018-09-28 22:32:32 +0400

    add help short (machine) status

--- functions.php
+++ functions.php
@@ -137,6 +137,44 @@ function git_status(){
 	$newtext[0] = ""; // add
 	$newtext[1] = ""; // 
 	$newtext[2] = "";
+
+
+/*
++	' ' = unmodified
++	M = modified
++	A = added
++	D = deleted
+	R = renamed
+	C = copied
+	U = updated but unmerged
+
+X          Y     Meaning
+-------------------------------------------------
+  [AMD]   not updated
+M        [ MD]   updated in index
+A        [ MD]   added to index
+D                deleted from index
+R        [ MD]   renamed in index
+C        [ MD]   copied in index
+[MARC]           index and work tree matches
+[ MARC]     M    work tree changed since index
+[ MARC]     D    deleted in work tree
+[ D]        R    renamed in work tree
+[ D]        C    copied in work tree
+-------------------------------------------------
+D           D    unmerged, both deleted
+A           U    unmerged, added by us
+U           D    unmerged, deleted by them
+U           A    unmerged, added by them
+D           U    unmerged, deleted by us
+A           A    unmerged, both added
+U           U    unmerged, both modified
+-------------------------------------------------
+?           ?    untracked
+!           !    ignored
+-------------------------------------------------
+*/
+
 	/* Добавленные */
 	$text = preg_replace_callback(
 		'/^A\s\s(.*)$/m',
